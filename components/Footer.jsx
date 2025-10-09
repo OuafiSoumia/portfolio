f3aaec245cb2f99849cc753dcb51fc334915c627
ExploTech <94236541+OuafiSoumia@users.noreply.github.com>
2025-10-09 19:43:32 +0200

    fixed contacts

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,10 +16,29 @@ const Footer = ({isDarkMode}) => {
 
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
         <p>© 2025 OS. All rights reserved.</p>
-        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://github.com/OuafiSoumia">GitHub</a></li>
-            <li><a target='_blank' href="https://www.linkedin.com/in/soumia-ouafi-a66438229/">LinkedIn</a></li>
+        <ul className="fixed bottom-4 right-4 flex  gap-10  ">
+          <li>
+            <a
+              href="https://github.com/OuafiSoumia"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-200 transition"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/soumia-ouafi-a66438229/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-200 transition"
+            >
+              LinkedIn
+            </a>
+          </li>
         </ul>
+
     </div>
 
     </div>
